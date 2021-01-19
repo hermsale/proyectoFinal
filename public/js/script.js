999fcaa1e1dbeed1e5e73cd87c1915ea10cd3964
hermsale <hermsale@gmail.com>
2021-01-19 13:03:10 -0300

    avances proyecto

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+
+
+let btn_agregarLocal = document.querySelector("#btn_agregarLocal");
+
+
+// let btn_cargar = document.querySelector("#btn-cargar");
+
+btn_agregarLocal.addEventListener("click",function(){
+    window.location="http://www.cristalab.com";
+    console.log("boton");
+})
+
+// window.locationf="http://www.cristalab.com";
